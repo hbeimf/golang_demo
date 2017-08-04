f7bba9b89f8b0132f298bb909bb39c265ba7c8b8
maofeng <maofeng@jrq.com>
2017-08-04 10:12:21 +0800

    chat

--- HttpServer/HttpServer.go
+++ HttpServer/HttpServer.go
@@ -21,6 +21,7 @@ import (
     "net/http"
     "./controller"
     "./model"
+    "log"
 )
 
 // 设置全局配置变量，并带默认值
@@ -41,20 +42,36 @@ func main() {
 
     ctrl := controller.GetControllerInstance()
 
+
     // 注册控制器函数
     http.HandleFunc("/get", ctrl.GetHandler)
     http.HandleFunc("/post", ctrl.PostHandler)
 
     // websocket echo demo
     http.HandleFunc("/websocket", ctrl.WebsocketHandler)
+
     //聊天室　demo
-    http.HandleFunc("/chat", ctrl.ChatHandler)
+    http.HandleFunc("/", serveHome)
+    http.HandleFunc("/ws", ctrl.ChatHandler)
 
 
     err := http.ListenAndServe(*globalHttpHost, nil)
     if err != nil {
         fmt.Println("err")
     }
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+    log.Println(r.URL)
+    if r.URL.Path != "/" {
+        http.Error(w, "Not found", 404)
+        return
+    }
+    if r.Method != "GET" {
+        http.Error(w, "Method not allowed", 405)
+        return
+    }
+    http.ServeFile(w, r, "home.html")
 }
 
 

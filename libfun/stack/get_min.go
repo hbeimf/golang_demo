dc39069d3ed45a12e8265b98248e2d16f23997f2
mao <5622876@qq.com>
2017-11-03 22:51:49 -0700

    l

--- libfun/stack/get_min.go
+++ libfun/stack/get_min.go
@@ -6,22 +6,68 @@ import (
 )
 
 type Min struct {
-	top llstack.Stack
-	min_top llstack.Stack
+	top *llstack.Stack
+	min_top *llstack.Stack
 }
 
+func (self *Min) push(ele int32) {
+	fmt.Println(ele)
+
+	// set val
+	self.top.Push(ele)
+
+	// set min
+	min_val, err := self.min_top.Pop()
+	if err {
+		self.min_top.Push(min_val)
+		if ele <= min_val.(int32)  {
+			self.min_top.Push(ele)
+		} else {
+			self.min_top.Push(min_val)	
+		}
+	} else {
+		self.min_top.Push(ele)
+	}
+	
+}
+
+func (self *Min) pop() (int32, int32) {
+	val, _ := self.top.Pop()
+	min_val, _ := self.min_top.Pop()
+
+	return val.(int32), min_val.(int32)
+}
+
+
+
 func Test() {
-	stack := llstack.New()  // empty
-	stack.Push(1)       // 1
-	stack.Push(2)       // 1, 2
-	fmt.Println("val", stack.Values())
-	stack.Values()      // 2, 1 (LIFO order)
-	_, _ = stack.Peek() // 2,true
-	_, _ = stack.Pop()  // 2, true
-	_, _ = stack.Pop()  // 1, true
-	_, _ = stack.Pop()  // nil, false (nothing to pop)
-	stack.Push(1)       // 1
-	stack.Clear()       // empty
-	stack.Empty()       // true
-	stack.Size()        // 0
+	// stack := llstack.New()  // empty
+	// stack.Push(1)       // 1
+	// stack.Push(2)       // 1, 2
+	// fmt.Println("val", stack.Values())
+	// stack.Values()      // 2, 1 (LIFO order)
+	// _, _ = stack.Peek() // 2,true
+	// _, _ = stack.Pop()  // 2, true
+	// _, _ = stack.Pop()  // 1, true
+	// _, _ = stack.Pop()  // nil, false (nothing to pop)
+	// stack.Push(1)       // 1
+	// stack.Clear()       // empty
+	// stack.Empty()       // true
+	// stack.Size()        // 0
+
+	min := &Min{
+		top: llstack.New(),
+		min_top:llstack.New(),
+	}
+
+	min.push(1)
+	min.push(2)
+	min.push(3)
+
+	// fmt.Println("stack:", min)
+
+	val, min_val := min.pop()
+
+	fmt.Println("pop:", val, min_val)
+
 }

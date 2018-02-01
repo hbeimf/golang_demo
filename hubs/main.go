1b4fe37f098ac71b6c294cf431acbd59c0da35d6
maomao <you@example.com>
2018-02-01 16:11:17 +0800

    hubs

---
+++ hubs/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	// "./list"
+	// "./stack"
+	// "./sort"
+	"fmt"
+)
+
+func main() {
+	// stack.Test()
+	// stack.TestGetMaxWindow()
+	// sort.Test()
+	fmt.Printf("Hello World!\n God Bless You!\n");
+
+}

06da8218b08c57ee552b86cfcd5cf415b608f6b5
maomao <5622876@qq.com>
2018-09-18 16:31:11 +0800

    t

--- tps/server.go
+++ tps/server.go
@@ -27,32 +27,32 @@ func main() {
 			var pbTest = new(pb.PbTest)
 			for {
 				// read request
-				var packet = socket.GetMessage(socket.WithNewBody(
+				var message = socket.GetMessage(socket.WithNewBody(
 					func(header socket.Header) interface{} {
 						*pbTest = pb.PbTest{}
 						return pbTest
 					}),
 				)
-				err = s.ReadMessage(packet)
+				err = s.ReadMessage(message)
 				if err != nil {
 					log.Printf("[SVR] read request err: %v", err)
 					return
 					// } else {
-					// log.Printf("[SVR] read request: %s", packet.String())
+					// log.Printf("[SVR] read request: %s", message.String())
 				}
 
 				// write response
 				pbTest.A = pbTest.A + pbTest.B
 				pbTest.B = pbTest.A - pbTest.B*2
-				packet.SetBody(pbTest)
+				message.SetBody(pbTest)
 
-				err = s.WriteMessage(packet)
+				err = s.WriteMessage(message)
 				if err != nil {
 					log.Printf("[SVR] write response err: %v", err)
 					// } else {
-					// log.Printf("[SVR] write response: %s", packet.String())
+					// log.Printf("[SVR] write response: %s", message.String())
 				}
-				socket.PutMessage(packet)
+				socket.PutMessage(message)
 			}
 		}(socket.GetSocket(conn))
 	}

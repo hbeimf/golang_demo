609869ea19c2b5d48e68466cb1127bdb5d100e39
maomao <5622876@qq.com>
2018-09-18 17:59:24 +0800

    x

--- mymap/main.go
+++ mymap/main.go
@@ -4,8 +4,21 @@ import (
 	"log"
 )
 
+type Students struct {
+	student map[int]*Student
+}
+
+func (ss *Students) Set(s *Student) {
+	ss.student[s.uid] = s
+}
+
+func (ss *Students) Get(uid int) *Student {
+	return ss.student[uid]
+}
+
 type Student struct {
-	id      int
+	uid     int
+	gid     int
 	name    string
 	address string
 	age     int
@@ -16,13 +29,15 @@ func main() {
 	m := make(map[int]*Student)
 
 	var s *Student = new(Student)
-	s.id = 101
+	s.uid = 101
+	s.gid = 1
 	s.name = "Mikle"
 	s.address = "红旗南路"
 	s.age = 18
 
-	m[s.id] = s
+	uid := 101
+	m[uid] = s
 
-	log.Printf("student: ... ", m[s.id].name, m[s.id].address, m[s.id].age)
+	log.Printf("student: ... ", m[uid].name, m[uid].address, m[uid].age)
 
 }

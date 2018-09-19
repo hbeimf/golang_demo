c2da4c8ec2082fcd858b47f1fc57e19ab05d96bb
maomao <5622876@qq.com>
2018-09-19 11:08:32 +0800

    x

--- mymap/main.go
+++ mymap/main.go
@@ -4,25 +4,49 @@ import (
 	"log"
 )
 
+// 学校 ====================================
 type School struct {
 	sid        int
 	name       string
 	class_room map[int]*ClassRoom
+	students   map[int]*Student
 }
 
+func (this *School) SetStudent(s *Student) {
+	this.students[s.uid] = s
+}
+
+func (this *School) GetStudent(uid int) *Student {
+	return this.students[uid]
+}
+
+func (this *School) SetClassRoom(c *ClassRoom) {
+	this.class_room[c.cid] = c
+}
+
+func (this *School) GetClassRoom(cid int) *ClassRoom {
+	return this.class_room[cid]
+}
+
+func (this *School) ChangeClassRoom(s *Student, cid int) {
+
+}
+
+// 教室 ====================================
 type ClassRoom struct {
-	cid     int
-	student map[int]*Student
+	cid      int
+	students map[int]*Student
 }
 
-func (c *ClassRoom) Set(s *Student) {
-	c.student[s.uid] = s
+func (this *ClassRoom) SetStudent(s *Student) {
+	this.students[s.uid] = s
 }
 
-func (c *ClassRoom) Get(uid int) *Student {
-	return c.student[uid]
+func (this *ClassRoom) GetStudent(uid int) *Student {
+	return this.students[uid]
 }
 
+// 学生 ====================================
 type Student struct {
 	uid           int
 	class_room_id int
@@ -31,6 +55,11 @@ type Student struct {
 	age           int
 }
 
+func (this *Student) ChangeClassRoom(cid int) {
+	this.class_room_id = cid
+}
+
+// run
 func main() {
 	log.Printf("mymap ... ")
 	m := make(map[int]*Student)
